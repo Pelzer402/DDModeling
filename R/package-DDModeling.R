bab251bc4e6fde267ed311877f5cc85c342bb780
Pelzer402 <thomas.pelzer.privat@gmail.com>
2020-02-14 16:55:19 +0100

    update

--- R/package-DDModeling.R
+++ R/package-DDModeling.R
@@ -1,6 +1,6 @@
 # Clean up .dll
 .onUnload <- function (libpath) {library.dynam.unload("DDModeling", libpath)}
-
+Sys.setenv("PKG_CXXFLAGS"="-O3 -std=c++14")
 #' DDModeling pakage
 #'
 #' A pakage for the easy integration of dirft diffusion models in cognitive psychology
