609cd712b0d657af4cbbc9b7272a8b64686d95d8
Pelzer402 <thomas.pelzer.privat@gmail.com>
2020-04-11 19:45:52 +0200

    misc

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,6 @@
 # Internal rescaling function
 unscale_scale <- function(IN = NULL,unscaler = NULL){
-  if (isnull(unscaler$Center) & is.null(unscaler$Stddev))
+  if (is.null(unscaler$Center) & is.null(unscaler$Stddev))
   {
     return(IN)
   }
